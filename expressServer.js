aa1133aa8214a8e414e5c4cc6e48c7bd900e5c5d
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-20 11:39:28 -0500

    core: creates expressServer file for handling CRUD operations

---
+++ expressServer.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+function readFile() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./pets.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      resolve(JSON.parse(data));
+    });
+  });
+}
+
+app.get('/pets', function (req, res) {
+  readFile()
+    .then((data) => {
+      let pets = data;
+      res.send(pets);
+    });
+});
+
+app.get('/pets/:id', function (req, res) {
+  readFile()
+    .then((data) => {
+      let pets = data;
+      let id = req.params.id
+
+      if (pets[id]) {
+        res.send(pets[id]);
+      } else {
+        res.status(404).send('Not Found');
+      }
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
