6867fc827137903939e567765ceb5a3b1f59b48a
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-20 14:18:27 -0500

    test: adds error handling and reject on file system request error

--- expressServer.js
+++ expressServer.js
@@ -1,26 +1,66 @@
 const express = require('express');
 const fs = require('fs');
+var bodyParser = require('body-parser');
 
 const app = express();
+
 
 function readFile() {
   return new Promise((resolve, reject) => {
     fs.readFile('./pets.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      resolve(JSON.parse(data));
+      // reject on errors, or resolve with data from file
+      err ? reject(err) : resolve(JSON.parse(data));
     });
   });
 }
 
-app.get('/pets', function (req, res) {
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+
+app.get('/pets', function (req, res, next) {
   readFile()
     .then((data) => {
       let pets = data;
       res.send(pets);
+    })
+    .catch((err) => {
+      next(err);
     });
 });
 
-app.get('/pets/:id', function (req, res) {
+app.post('/pets', jsonParser, function (req, res, next) {
+  if (!req.body) return res.sendStatus(400);
+
+  readFile()
+    .then((data) => {
+      let pets = data;
+
+      if (req.body.name && !Number.isNaN(req.body.age) && req.body.kind) {
+        let newPet = {
+          name: req.body.name,
+          age: req.body.age,
+          kind: req.body.kind
+        };
+
+        pets.push(newPet);
+        fs.writeFile('./pets.json', JSON.stringify(pets), (err) => {
+          if (err) { return next(err); }
+        });
+
+        res.send(newPet);
+      } else {
+        res.set('Content-Type', 'text/plain');
+        res.status(400).send('Bad Request');
+      }
+
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.get('/pets/:id', function (req, res, next) {
   readFile()
     .then((data) => {
       let pets = data;
@@ -32,11 +72,25 @@ app.get('/pets/:id', function (req, res) {
         res.set('Content-Type', 'text/plain');
         res.status(404).send('Not Found');
       }
+    })
+    .catch((err) => {
+      next(err);
     });
 });
 
+app.use(function (req, res, next) {
+  res.set('Content-Type', 'text/plain');
+  res.status(404).send('Not Found');
+});
+
+app.use(function(err, req, res, next) {
+    console.error('error handler hit');
+    res.status(err.status || 500);
+    res.send('Internal Server Error');
+  });
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
 
 module.exports = app;
